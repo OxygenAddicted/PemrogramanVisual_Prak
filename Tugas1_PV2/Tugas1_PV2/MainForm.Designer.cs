efec4af8002c4bbf497c9d5aea928f2ea5d37119
Al Imamul Luthfi <86113150+OxygenAddicted@users.noreply.github.com>
2022-04-26 23:37:21 +0700

    Add files via upload

---
+++ Tugas1_PV2/Tugas1_PV2/MainForm.Designer.cs
@@ -0,0 +1,203 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: user
+ * Date: 4/26/2022
+ * Time: 9:41 PM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+namespace Tugas1_PV2
+{
+	partial class MainForm
+	{
+		/// <summary>
+		/// Designer variable used to keep track of non-visual components.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+		private System.Windows.Forms.DataGridView dataGridView1;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.TextBox textBoxNim;
+		private System.Windows.Forms.TextBox textBoxNama;
+		private System.Windows.Forms.TextBox textBoxKom;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.Label label4;
+		private System.Windows.Forms.Button btnInsert;
+		private System.Windows.Forms.Button btnUpdate;
+		private System.Windows.Forms.Button btnDelete;
+		
+		/// <summary>
+		/// Disposes resources used by the form.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing) {
+				if (components != null) {
+					components.Dispose();
+				}
+			}
+			base.Dispose(disposing);
+		}
+		
+		/// <summary>
+		/// This method is required for Windows Forms designer support.
+		/// Do not change the method contents inside the source code editor. The Forms designer might
+		/// not be able to load this method if it was changed manually.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.dataGridView1 = new System.Windows.Forms.DataGridView();
+			this.label1 = new System.Windows.Forms.Label();
+			this.textBoxNim = new System.Windows.Forms.TextBox();
+			this.textBoxNama = new System.Windows.Forms.TextBox();
+			this.textBoxKom = new System.Windows.Forms.TextBox();
+			this.label2 = new System.Windows.Forms.Label();
+			this.label3 = new System.Windows.Forms.Label();
+			this.label4 = new System.Windows.Forms.Label();
+			this.btnInsert = new System.Windows.Forms.Button();
+			this.btnUpdate = new System.Windows.Forms.Button();
+			this.btnDelete = new System.Windows.Forms.Button();
+			((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// dataGridView1
+			// 
+			this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+			this.dataGridView1.Location = new System.Drawing.Point(362, 126);
+			this.dataGridView1.Name = "dataGridView1";
+			this.dataGridView1.Size = new System.Drawing.Size(404, 263);
+			this.dataGridView1.TabIndex = 0;
+			this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.DataGridView1CellContentClick);
+			// 
+			// label1
+			// 
+			this.label1.Font = new System.Drawing.Font("Montserrat", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label1.ImageAlign = System.Drawing.ContentAlignment.TopLeft;
+			this.label1.Location = new System.Drawing.Point(188, 9);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(333, 70);
+			this.label1.TabIndex = 1;
+			this.label1.Text = "Pangkalan Data Mahasiswa Prodi Ilmu Komputer 2020";
+			this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+			// 
+			// textBoxNim
+			// 
+			this.textBoxNim.Font = new System.Drawing.Font("Arial Narrow", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.textBoxNim.Location = new System.Drawing.Point(145, 130);
+			this.textBoxNim.Name = "textBoxNim";
+			this.textBoxNim.Size = new System.Drawing.Size(176, 25);
+			this.textBoxNim.TabIndex = 2;
+			// 
+			// textBoxNama
+			// 
+			this.textBoxNama.Font = new System.Drawing.Font("Arial Narrow", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.textBoxNama.Location = new System.Drawing.Point(145, 167);
+			this.textBoxNama.Name = "textBoxNama";
+			this.textBoxNama.Size = new System.Drawing.Size(176, 25);
+			this.textBoxNama.TabIndex = 3;
+			// 
+			// textBoxKom
+			// 
+			this.textBoxKom.Font = new System.Drawing.Font("Arial Narrow", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.textBoxKom.Location = new System.Drawing.Point(145, 206);
+			this.textBoxKom.Name = "textBoxKom";
+			this.textBoxKom.Size = new System.Drawing.Size(176, 25);
+			this.textBoxKom.TabIndex = 4;
+			// 
+			// label2
+			// 
+			this.label2.Font = new System.Drawing.Font("Arial Narrow", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label2.Location = new System.Drawing.Point(54, 132);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(45, 23);
+			this.label2.TabIndex = 5;
+			this.label2.Text = "NIM";
+			// 
+			// label3
+			// 
+			this.label3.Font = new System.Drawing.Font("Arial Narrow", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label3.Location = new System.Drawing.Point(54, 167);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(45, 23);
+			this.label3.TabIndex = 6;
+			this.label3.Text = "Nama";
+			// 
+			// label4
+			// 
+			this.label4.Font = new System.Drawing.Font("Arial Narrow", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label4.Location = new System.Drawing.Point(54, 206);
+			this.label4.Name = "label4";
+			this.label4.Size = new System.Drawing.Size(45, 23);
+			this.label4.TabIndex = 7;
+			this.label4.Text = "KOM";
+			// 
+			// btnInsert
+			// 
+			this.btnInsert.BackColor = System.Drawing.SystemColors.InactiveCaption;
+			this.btnInsert.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+			this.btnInsert.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+			this.btnInsert.Font = new System.Drawing.Font("Arial Narrow", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.btnInsert.Location = new System.Drawing.Point(49, 270);
+			this.btnInsert.Name = "btnInsert";
+			this.btnInsert.Size = new System.Drawing.Size(75, 34);
+			this.btnInsert.TabIndex = 8;
+			this.btnInsert.Text = "INSERT";
+			this.btnInsert.UseVisualStyleBackColor = false;
+			this.btnInsert.Click += new System.EventHandler(this.BtnInsertClick);
+			// 
+			// btnUpdate
+			// 
+			this.btnUpdate.BackColor = System.Drawing.SystemColors.Info;
+			this.btnUpdate.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+			this.btnUpdate.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+			this.btnUpdate.Font = new System.Drawing.Font("Arial Narrow", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.btnUpdate.Location = new System.Drawing.Point(149, 269);
+			this.btnUpdate.Name = "btnUpdate";
+			this.btnUpdate.Size = new System.Drawing.Size(75, 34);
+			this.btnUpdate.TabIndex = 9;
+			this.btnUpdate.Text = "UPDATE";
+			this.btnUpdate.UseVisualStyleBackColor = false;
+			this.btnUpdate.Click += new System.EventHandler(this.BtnUpdateClick);
+			// 
+			// btnDelete
+			// 
+			this.btnDelete.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+			this.btnDelete.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+			this.btnDelete.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+			this.btnDelete.Font = new System.Drawing.Font("Arial Narrow", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.btnDelete.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+			this.btnDelete.Location = new System.Drawing.Point(246, 268);
+			this.btnDelete.Name = "btnDelete";
+			this.btnDelete.Size = new System.Drawing.Size(75, 34);
+			this.btnDelete.TabIndex = 10;
+			this.btnDelete.Text = "DELETE";
+			this.btnDelete.UseVisualStyleBackColor = false;
+			this.btnDelete.Click += new System.EventHandler(this.BtnDeleteClick);
+			// 
+			// MainForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+			this.ClientSize = new System.Drawing.Size(778, 411);
+			this.Controls.Add(this.btnDelete);
+			this.Controls.Add(this.btnUpdate);
+			this.Controls.Add(this.btnInsert);
+			this.Controls.Add(this.label4);
+			this.Controls.Add(this.label3);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.textBoxKom);
+			this.Controls.Add(this.textBoxNama);
+			this.Controls.Add(this.textBoxNim);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.dataGridView1);
+			this.Name = "MainForm";
+			this.Text = "Database Akademik";
+			((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+	}
+}
---
+++ Tugas1_PV2/Tugas1_PV2/MainForm.cs
@@ -0,0 +1,144 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: user
+ * Date: 4/26/2022
+ * Time: 9:41 PM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Data.OleDb;
+using MySql.Data;
+using MySql.Data.MySqlClient;
+using System.Data;
+
+namespace Tugas1_PV2
+{
+	
+	public partial class MainForm : Form
+	{
+		MySqlConnection co = new MySqlConnection(@"server=localhost; userid=root ; password=; database=akademik;");
+		MySqlCommand mycommand = new MySqlCommand();
+		MySqlDataAdapter myadapter = new MySqlDataAdapter();
+		public MainForm()
+		{
+			InitializeComponent();
+			co.Open();
+			ReadData();
+		}
+		
+		public void ReadData()
+		{
+			try{
+				mycommand.Connection = co;
+				myadapter.SelectCommand = mycommand;
+				mycommand.CommandText = "select * from data_mahasiswa";
+				DataSet ds = new DataSet();
+				
+				if (myadapter.Fill(ds,"dftpesan")>0)
+				{
+					dataGridView1.DataSource = ds;
+					dataGridView1.DataMember = "dftpesan";
+				}
+			}
+			catch (Exception ex)
+			{
+				MessageBox.Show(ex.ToString());
+			}
+		}
+		
+		public void InsertData()
+		{
+			try
+			{
+				mycommand.Connection=co;
+				mycommand.CommandText="insert into data_mahasiswa values('"+textBoxNim.Text+"','"+textBoxNama.Text+"','"+textBoxKom.Text+"')";
+				myadapter.SelectCommand= mycommand;
+				if (mycommand.ExecuteNonQuery()==1)
+				{
+					MessageBox.Show("Data berhasil dimasukan","Informasi",MessageBoxButtons.OK,MessageBoxIcon.Information);
+					ReadData();
+					}
+			}
+			catch(Exception ex)
+			{
+				MessageBox.Show(ex.ToString());
+			}
+		}
+		
+		public void UpdateData()
+		{
+			try{
+				mycommand.CommandText = "update data_mahasiswa set nim='"+textBoxNim.Text+"',nama='"+textBoxNama.Text+"',kom='"+textBoxKom.Text+"' where nim ='"+textBoxNim.Text+"'";
+				myadapter.SelectCommand = mycommand;
+				if (mycommand.ExecuteNonQuery()==1)
+				{
+					MessageBox.Show("Data berhasil diupdate","Informasi",MessageBoxButtons.OK,MessageBoxIcon.Information);
+					ReadData();
+				}
+			}
+			catch(Exception ex)
+			{
+				MessageBox.Show(ex.ToString());
+			}
+		}
+		
+		public void DeleteData()
+		{
+			try
+			{
+				mycommand.CommandText="delete from data_mahasiswa where nim='"+textBoxNim.Text+"'";
+				myadapter.SelectCommand = mycommand;
+				if (mycommand.ExecuteNonQuery()==1)
+				{
+					MessageBox.Show("Data berhasil dihapus","Informasi",MessageBoxButtons.OK,MessageBoxIcon.Information);
+					ReadData();
+				}
+			}
+			catch(Exception ex)
+			{
+				MessageBox.Show(ex.ToString());
+			}
+		}
+		
+		public bool dbOpen()
+		{
+			try
+			{
+				co.Open();
+			}
+			catch
+			{
+				MessageBox.Show("Tidak Terhubung");
+				return false;
+			}
+			return true;
+		}
+		
+		void BtnInsertClick(object sender, EventArgs e)
+		{
+			InsertData();
+		}
+		
+		void BtnUpdateClick(object sender, EventArgs e)
+		{
+			UpdateData();
+		}
+		
+		void BtnDeleteClick(object sender, EventArgs e)
+		{
+			DeleteData();
+		}
+		
+		void DataGridView1CellContentClick(object sender, DataGridViewCellEventArgs e)
+		{
+			textBoxNim.Text = dataGridView1.Rows[e.RowIndex].Cells[0].Value.ToString();
+			textBoxNama.Text = dataGridView1.Rows[e.RowIndex].Cells[1].Value.ToString();
+			textBoxKom.Text = dataGridView1.Rows[e.RowIndex].Cells[2].Value.ToString();
+		}
+		
+	}
+}
